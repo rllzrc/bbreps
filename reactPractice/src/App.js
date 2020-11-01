c809375c9503276c4276537b07a3c8706e2f32a8
rllzrc <rellas.email@gmail.com>
2020-11-01 15:00:26 -0500

    build: Add tooling to RP folder eslint prettier etc

--- reactPractice/src/App.js
+++ reactPractice/src/App.js
@@ -1,38 +1,33 @@
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
   ]);
-}
+};
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: 'something-important'}, [
-      React.createElement('h1', {}, 'Adopt Me!'),
-      React.createElement(Pet, {
-        name: 'Dalton',
-        animal: 'Dog',
-        breed: 'Shih-Tzu'
-      }),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'Havanese'
-      }),
-      React.createElement(Pet, {
-        name: 'Jack Bauer',
-        animal: 'Cat',
-        breed: 'Mixed'
-      })
-    ]);
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Dalton",
+      animal: "Dog",
+      breed: "Shih-Tzu",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Jack Bauer",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
 };
 
 // created app but it won't do anything until you render or "Create" it
 
 // will stamp App or string to create markup that will output to the DOM
 
-ReactDOM.render(
-  React.createElement(App), 
-  document.getElementById('root')
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
