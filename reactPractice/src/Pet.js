14975dbc943de64d1a4fdbf1483ac92237cbfbd1
rllzrc <rellas.email@gmail.com>
2020-11-01 15:51:52 -0500

    build: Add babel to + additional tooling settings to transpile JSX

---
+++ reactPractice/src/Pet.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+const Pet = ({ name, animal, breed }) => {
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, name),
+  //   React.createElement("h2", {}, animal),
+  //   React.createElement("h2", {}, breed)
+  // ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+}
+
+export default Pet;
