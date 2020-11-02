e4835a7890c7d409f07cff4e6833c75507fb4bb8
rllzrc <rellas.email@gmail.com>
2020-11-01 22:20:25 -0500

    build: Add ErrorBoundary to Details comp

--- reactPractice/src/Details.js
+++ reactPractice/src/Details.js
@@ -1,46 +1,92 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
+
 // const Details = () => {
 //   return <h1>hi lulz</h1>
 // };
 
 // turning function to class component
 
+// class Details extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       loading: true
+//     };
+//   }
+//   // like useEffect, runs once though unlike effect
+//   // great for AJAX requests
+//   componentDidMount () {
+//     pet.animal(this.props.id)
+//     .then(({ animal }) => {
+//       this.setState({
+//         name: animal.name,
+//         animal: animal.type,
+//         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+//         description: animal.description,
+//         media: animal.photos,
+//         breed: animal.breeds.primary,
+//         loading: false
+//       })
+//     }, console.error);
+//   }
+//   render () {
+//     if(this.state.loading) {
+//       return <h1>Loading ~~ ...</h1>
+//     }
+
+//     const {animal, breed, location, description, name, media} = this.state;
+
+//     return (
+//       <div className='details'>
+//         <Carousel media={media} />
+//         <div>
+//           <h1>{animal}</h1>
+//           <h2>{`${animal} - ${breed} - ${location}`}</h2>
+//           <button>Adopt {name}</button>
+//           <p>{description}</p>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Details;
+
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  }
-  // like useEffect, runs once though unlike effect
-  // great for AJAX requests
-  componentDidMount () {
-    pet.animal(this.props.id)
-    .then(({ animal }) => {
-      this.setState({
-        name: animal.name,
-        animal: animal.type,
-        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-        description: animal.description,
-        media: animal.photos,
-        breed: animal.breeds.primary,
-        loading: false
+  state = { loading: true };
+  componentDidMount() {
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${
+            animal.contact.address.state
+          }`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false
+        });
       })
-    }, console.error);
+      .catch(err => this.setState({ error: err }));
   }
-  render () {
-    if(this.state.loading) {
-      return <h1>Loading ~~ ...</h1>
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading … ~~</h1>;
     }
 
-    const {animal, breed, location, description, name} = this.state;
+    const { animal, breed, location, description, media, name } = this.state;
 
     return (
-      <div className='details'>
+      <div className="details">
+        <Carousel media={media} />
         <div>
-          <h1>{animal}</h1>
-          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
           <button>Adopt {name}</button>
           <p>{description}</p>
         </div>
@@ -48,4 +94,11 @@ class Details extends React.Component {
     );
   }
 }
-export default Details;
+
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
